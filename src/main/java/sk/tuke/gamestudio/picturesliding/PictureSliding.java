1cb60f3b47ef22a9b6b81e1bbd7b746e72b8b48d
Nikita Munko <nikmoon16@gmail.com>
2025-03-24 23:27:29 +0100

    connect database for services

--- src/main/java/sk/tuke/gamestudio/picturesliding/PictureSliding.java
+++ src/main/java/sk/tuke/gamestudio/picturesliding/PictureSliding.java
@@ -1,8 +1,12 @@
 package sk.tuke.gamestudio.picturesliding;
 
-
+import sk.tuke.gamestudio.entity.Score;
 import sk.tuke.gamestudio.picturesliding.consoleui.ConsoleUI;
 import sk.tuke.gamestudio.picturesliding.core.GameField;
+import sk.tuke.gamestudio.service.ScoreServiceJDBC;
+
+import java.security.Provider;
+import java.util.Date;
 
 public class PictureSliding {
 
@@ -14,5 +18,7 @@ public class PictureSliding {
         // 4,3/10 - когда фиксану генерацию поля и мелкие туду, оценка выше будет
         // 2 - сервисы
         // 0,7 - тесты к сервисам
+//        ScoreServiceJDBC scoreServiceJDBC = new ScoreServiceJDBC();
+//        scoreServiceJDBC.addScore(new Score("picture_sliding", "Nikita Munko", 445, new Date()));
     }
 }
