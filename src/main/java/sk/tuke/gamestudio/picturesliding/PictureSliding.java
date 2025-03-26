6d27597f9ecaa015e98404af4e11592da06779df
Nikita Munko <nikita.munko@student.tuke.sk>
2025-03-26 22:22:40 +0100

    test commit

--- src/main/java/sk/tuke/gamestudio/picturesliding/PictureSliding.java
+++ src/main/java/sk/tuke/gamestudio/picturesliding/PictureSliding.java
@@ -1,24 +1,15 @@
 package sk.tuke.gamestudio.picturesliding;
 
-import sk.tuke.gamestudio.entity.Comment;
-import sk.tuke.gamestudio.entity.Rating;
-import sk.tuke.gamestudio.entity.Score;
 import sk.tuke.gamestudio.picturesliding.consoleui.ConsoleUI;
 import sk.tuke.gamestudio.picturesliding.core.GameField;
-import sk.tuke.gamestudio.service.CommentServiceJDBC;
-import sk.tuke.gamestudio.service.RatingServiceJDBC;
-import sk.tuke.gamestudio.service.ScoreServiceJDBC;
-
-import java.security.Provider;
-import java.util.Date;
 
 public class PictureSliding {
 
     public static void main(String[] args) {
-//        var gameField = new GameField(2, 2);
-//        var consoleUI = new ConsoleUI(gameField);
-//        consoleUI.play();
-//        ConsoleUI.closeScanner();
+        var gameField = new GameField(2, 2);
+        var consoleUI = new ConsoleUI(gameField);
+        consoleUI.play();
+        ConsoleUI.closeScanner();
 
 //        ScoreServiceJDBC scoreServiceJDBC = new ScoreServiceJDBC();
 //        scoreServiceJDBC.addScore(new Score("picture_sliding", "Nikita Munko", 445, new Date()));
@@ -26,7 +17,7 @@ public class PictureSliding {
 //        CommentServiceJDBC commentServiceJDBC = new CommentServiceJDBC();
 //        commentServiceJDBC.addComment(new Comment("picture_sliding", "HGC", "lol)))", new Date()));
 
-        RatingServiceJDBC ratingServiceJDBC = new RatingServiceJDBC();
-        ratingServiceJDBC.setRating(new Rating("picture_sliding", "Nikita Muno", 999, new Date()));
+//        RatingServiceJDBC ratingServiceJDBC = new RatingServiceJDBC();
+//        ratingServiceJDBC.setRating(new Rating("picture_sliding", "Nikita Muno", 999, new Date()));
     }
 }
