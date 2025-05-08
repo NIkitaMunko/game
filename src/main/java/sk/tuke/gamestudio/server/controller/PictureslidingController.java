679115ef0bb952063bafb1f4f4f728c556441c97
Nikita Munko <nikmoon16@gmail.com>
2025-05-08 21:33:51 +0200

    few refactoring, add ability to add rating

--- src/main/java/sk/tuke/gamestudio/server/controller/PictureslidingController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/PictureslidingController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
 import sk.tuke.gamestudio.entity.Comment;
+import sk.tuke.gamestudio.entity.Rating;
 import sk.tuke.gamestudio.picturesliding.core.Direction;
 import sk.tuke.gamestudio.picturesliding.core.GameField;
 import sk.tuke.gamestudio.picturesliding.core.Tile;
 import sk.tuke.gamestudio.service.CommentService;
+import sk.tuke.gamestudio.service.RatingService;
 
 import java.util.Date;
 
@@ -25,12 +27,16 @@ public class PictureslidingController {
     @Autowired
     private CommentService commentService;
 
+    @Autowired
+    private RatingService ratingService;
+
     @RequestMapping("/picturesliding")
     public String picturesliding(
             @RequestParam(value = "direction", required = false) String direction,
             @RequestParam(value = "reset", required = false) boolean reset,
             @RequestParam(value = "playerName", required = false) String inputPlayerName,
             @RequestParam(value = "comment", required = false) String comment,
+            @RequestParam(value = "rating", required = false) String rating,
             Model model
     ) {
 
@@ -39,14 +45,21 @@ public class PictureslidingController {
         if (comment != null && playerName != null)
             commentService.addComment(new Comment("picture_sliding", playerName, comment, new Date()));
 
+        if (rating != null && playerName != null) {
+            try {
+                int gameRate = Integer.parseInt(rating);
+                ratingService.setRating(new Rating("picture_sliding", playerName, gameRate, new Date()));
+            } catch (NumberFormatException e) {
+                model.addAttribute("error", "Invalid rating value.");
+            }
+        }
+
         if (reset) {
             field = new GameField(3, 3);
         } else if (direction != null && !direction.isEmpty()) {
             Direction dir = Direction.getDirection(direction.toLowerCase());
             if (dir != null) field.moveTile(dir);
         }
-
-        // todo добавить поддержку взаимодействия с датабазой и её вывод на сайт
 
         Tile[][] fieldArray = field.getFieldArray();
         int rows = fieldArray.length;
@@ -68,6 +81,7 @@ public class PictureslidingController {
         model.addAttribute("frameNumbers", frameNumbers);
         model.addAttribute("isSolved", field.isSolved());
         model.addAttribute("comments", commentService.getComments("picture_sliding"));
+        model.addAttribute("rating", ratingService.getAverageRating("picture_sliding"));
         model.addAttribute("playerName", this.playerName);
 
         return "ps"; // todo сделать чтобы сайт каждый раз сам не перерисовывался
