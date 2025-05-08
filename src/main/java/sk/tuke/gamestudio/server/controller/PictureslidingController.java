f569a8a962e79269c63ea1299d2645ed41f48c7b
Nikita Munko <nikmoon16@gmail.com>
2025-05-08 19:15:01 +0200

    add comments table at web service

--- src/main/java/sk/tuke/gamestudio/server/controller/PictureslidingController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/PictureslidingController.java
@@ -1,20 +1,28 @@
 package sk.tuke.gamestudio.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
+import sk.tuke.gamestudio.entity.Comment;
 import sk.tuke.gamestudio.picturesliding.core.Direction;
 import sk.tuke.gamestudio.picturesliding.core.GameField;
 import sk.tuke.gamestudio.picturesliding.core.Tile;
+import sk.tuke.gamestudio.service.CommentService;
+
+import java.util.List;
 
 @Controller
 @Scope(WebApplicationContext.SCOPE_SESSION)
 public class PictureslidingController {
 
     private GameField field = new GameField(3, 3);
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping("/picturesliding")
     public String picturesliding(
@@ -23,9 +31,9 @@ public class PictureslidingController {
             Model model
     ) {
 
-        if (reset) field = new GameField(3, 3);
-
-        if (direction != null && !direction.isEmpty()) {
+        if (reset) {
+            field = new GameField(3, 3);
+        } else if (direction != null && !direction.isEmpty()) {
             Direction dir = Direction.getDirection(direction.toLowerCase());
             if (dir != null) field.moveTile(dir);
         }
@@ -51,6 +59,7 @@ public class PictureslidingController {
         model.addAttribute("field", values);
         model.addAttribute("frameNumbers", frameNumbers);
         model.addAttribute("isSolved", field.isSolved());
+        model.addAttribute("comments", commentService.getComments("picture_sliding"));
 
         return "ps"; // todo сделать чтобы сайт каждый раз сам не перерисовывался
     }
